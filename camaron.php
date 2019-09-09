d9fb7d4e1b9568b6a35fee3d87b0738ae98b7f74
Javier Felix <desarrollo.ti@CORPORATIVOOJAI.COM>
2019-09-09 12:52:57 -0700

    Se agrego texto justificado

--- camaron.php
+++ camaron.php
@@ -9,7 +9,7 @@
     <div class="col-lg-10 spce-c">
         <div class="col-lg-8">
             <img class="p-alimentos" src="http://www.ojaialimentos.com.mx/img/secciones/cam_granjas.jpg" alt="alimentos">
-            <p class="pt-3" align="justify">
+            <p class="pt-3 txt-justify">
             La división acuícola de Ojai Alimentos es la más nueva de nuestra empresa, se inició su construcción en 1998 y arrancó su operación productiva a principios del año 2000 en una granja ubicada en Bahía de Kino, Sonora.
             Actualmente Ojai Alimentos cuenta con cuatro granjas acuícolas, de las cuales tres de ellas están en Sinaloa muy cerca de la capital del estado. En total contamos con una superficie de 1,472 hectáreas de espejo de agua con una capacidad de producción de un poco más de 4,400 toneladas métricas por ciclo.
             Ojai Alimentos siempre se ha distinguido por la gran calidad de sus productos y sus procesos, la división acuícola no es la excepción, ejemplo de ello es la certificación que obtuvo la Granja Acuícola Kino que fue reconocida  por sus “Buenas prácticas en producción de camarón” por parte del Servicio Nacional de Sanidad, Inocuidad y Calidad Agroalimentaria (SENASICA); este reconocimiento solo se ha otorgado a tres granjas en todo el país incluyendo la nuestra.
@@ -29,7 +29,7 @@
     <div class="col-lg-10 spce-c">
         <div class="col-lg-8">
             <img class="p-alimentos" src="http://www.ojaialimentos.com.mx/img/secciones/cam_empacadora.jpg" alt="alimentos">
-            <p class="pt-3" align="justify">
+            <p class="pt-3 txt-justify">
             En  julio del 2004 Ojai Alimentos arrancó la operación de una planta de congeladora de camarón con una capacidad inicial de producción de 1.4 millones de libras por año de camarón fresco para ser congelado sin cabeza y empacado en caja top-open en presentaciones de 4 o 5 libras. En el año 2006 se logró un nivel de producción de 4 millones de libras y el objetivo es seguir creciendo consistentemente en los próximos años. A un par de años de haber arrancado esta operación se inició con la ampliación de la misma y próximamente quedará concluida la plata de Valor Agregado, lo que nos permitirá explorar nuevos segmentos de mercado a través de productos especiales.
             Toda nuestra producción de camarón proviene de cultivo de nuestras propias granjas, esto garantiza la rastreabilidad y el control total desde la siembra, cosecha y proceso; obteniendo un camarón de inmejorable calidad, sabor, textura y color.       
         </p>
@@ -49,7 +49,7 @@
     <div class="col-lg-10 spce-c">
         <div class="col-lg-8">
             <img class="p-alimentos" src="http://www.ojaialimentos.com.mx/img/secciones/cer_gran.jpg" alt="genetica">
-            <p class="pt-3" align="justify">
+            <p class="pt-3 txt-justify">
         OJAI Alimentos cuenta con 17 granjas de postura comercial, aquí es donde tenemos a nuestras aves en edad productiva que producen huevo blanco para plato, las cuales están ubicadas principalmente en el valle del yaqui muy cerca de Cd. Obregón donde tenemos el 55% de nuestra producción con 1’077,560 aves, además de nuestras granjas en Hermosillo, donde tenemos el 13% de la producción gracias a 258,000 aves y 634,980 aves en Los Mochis, que representan el 32 % de nuestra producción total.
         Contamos con capacidad para albergar 1’970,540 aves que nos permiten tener una producción anual de 31, 000 toneladas de huevo.
         Es de suma importancia reiterar que el 100% de nuestras granjas tienen la certificación por parte de SAGARPA de libres enfermedades esxóticas como influenza aviar, Newcastle y salmonella.
--- huevo.php
+++ huevo.php
@@ -9,7 +9,7 @@
     <div class="col-lg-10 spce-c">
         <div class="col-lg-8">
             <img class="p-alimentos" src="http://www.ojaialimentos.com.mx/img/secciones/cer_alime.jpg" alt="alimentos">
-            <p class="pt-3">
+            <p class="pt-3 txt-justify">
             En Ojai Alimentos contamos con la infraestructura necesaria para producir el alimento balanceado utilizado en nuestras granjas de cerdo y aves. Para ello contamos con tres plantas localizadas en puntos estratégicos para abastecer nuestras operaciones. En estas unidades se lleva a cabo el proceso desde de molienda del grano y el mezclado de los ingredientes con estrictos controles de que nos permiten producir un alimento final de gran calidad.
 
 Dos de nuestras plantas se encuentran en el estado de Sonora y una más en el estado de Sinaloa, en conjunto tienen una capacidad de producción mensual de 14,000 toneladas de alimentos balanceados así como las instalaciones necesarias para almacenar 15,000 toneladas de grano.
@@ -29,7 +29,7 @@ Dos de nuestras plantas se encuentran en el estado de Sonora y una más en el es
     <div class="col-lg-10 spce-c">
         <div class="col-lg-8">
             <img class="p-alimentos" src="http://www.ojaialimentos.com.mx/img/secciones/hue_crianza2.jpg" alt="alimentos">
-            <p class="pt-3">
+            <p class="pt-3 txt-justify">
             Para iniciar con el proceso de producción de huevo en Ojai Alimentos se cuenta con cuatro granjas de crianza donde nuestras aves permanecen desde el primer día de vida hasta las 18 semanas de edad. En esta etapa, el ave pasa de ser pollita a gallina joven, llega a su madurez sexual y se prepara para iniciar su vida productiva. Se tiene una capacidad de crianza de 510,000 pollitas en dos unidades donde se están desarrollando las futuras productoras de huevo de nuestra compañía. </p>
         </div>
       
@@ -46,7 +46,7 @@ Dos de nuestras plantas se encuentran en el estado de Sonora y una más en el es
     <div class="col-lg-10 spce-c">
         <div class="col-lg-8">
             <img class="p-alimentos" src="http://www.ojaialimentos.com.mx/img/secciones/hue_postura.jpg" alt="alimentos">
-            <p class="pt-3">
+            <p class="pt-3 txt-justify">
         OJAI Alimentos cuenta con 17 granjas de postura comercial, aquí es donde tenemos a nuestras aves en edad productiva que producen huevo blanco para plato, las cuales están ubicadas principalmente en el valle del yaqui muy cerca de Cd. Obregón donde tenemos el 55% de nuestra producción con 1’077,560 aves, además de nuestras granjas en Hermosillo, donde tenemos el 13% de la producción gracias a 258,000 aves y 634,980 aves en Los Mochis, que representan el 32 % de nuestra producción total.
         Contamos con capacidad para albergar 1’970,540 aves que nos permiten tener una producción anual de 31, 000 toneladas de huevo.
         Es de suma importancia reiterar que el 100% de nuestras granjas tienen la certificación por parte de SAGARPA de libres enfermedades esxóticas como influenza aviar, Newcastle y salmonella.
