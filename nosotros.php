131fcfdf74780e99ac204f8ed5144b0235f1d8d1
Javier Felix <desarrollo.ti@CORPORATIVOOJAI.COM>
2020-04-28 13:10:12 -0700

    Se mejoro la seccion de distribuciones

--- nosotros.php
+++ nosotros.php
@@ -3,195 +3,179 @@
 
 
 <section class="col-lg-10 mx-auto pt-2 spce-c m-5">
-        <span class="content-titles">
-        <h3 class="title-center txt-center">HISTORIA</h3>
-        </span>
+   <span class="content-titles">
+      <h3 class="title-center txt-center">HISTORIA</h3>
+   </span>
 </section>
 <!-- Empieza seccion -->
-    <div class="spce-c mb-5">
-      <div class="col-lg-4 mgt-2">
-          <div class="col-lg-10 txt-justify">
-            <p > Ojai se fundó en 1979 como una empresa productora de huevo, pertenece a uno de los grupos con más solidez en la industria pecuaria del estado de sonora que desde sus orígenes se ha caracterizado por su orientación a producir alimentos de calidad.
-                </p>
-            <p >       En 1984 la empresa incursionó en la industria porcina como productor de cerdo y a finales de la década de los 90´s se decidió invertir en una planta para sacrificar y procesar cortes de alta calidad.
-          </p>
-              <p >
-                  En el año 2000 la empresa da otro importante paso al adquirir las primeras granjas acuícolas para producir camarón, con lo que se consolidó la tercera línea de productos de la compañía.
-                  Actualmente Grupo Ojai produce y comercializa huevo, carne de cerdo y camarón con los más altos estándares de calidad, lo que le ha permitido llegar a una gran parte de la República Mexicana, así como a mercados internacionales en Asia y América.
-              </p> 
-          </div>
+<div class="spce-c mb-5">
+   <div class="col-lg-4 mgt-2">
+      <div class="col-lg-10 txt-justify">
+         <p > Ojai se fundó en 1979 como una empresa productora de huevo, pertenece a uno de los grupos con más solidez en la industria pecuaria del estado de sonora que desde sus orígenes se ha caracterizado por su orientación a producir alimentos de calidad.
+         </p>
+         <p >       En 1984 la empresa incursionó en la industria porcina como productor de cerdo y a finales de la década de los 90´s se decidió invertir en una planta para sacrificar y procesar cortes de alta calidad.
+         </p>
+         <p >
+            En el año 2000 la empresa da otro importante paso al adquirir las primeras granjas acuícolas para producir camarón, con lo que se consolidó la tercera línea de productos de la compañía.
+            Actualmente Grupo Ojai produce y comercializa huevo, carne de cerdo y camarón con los más altos estándares de calidad, lo que le ha permitido llegar a una gran parte de la República Mexicana, así como a mercados internacionales en Asia y América.
+         </p>
       </div>
-        <div class="col-lg-4 content-image">
-        <img class="d-block w-100" src="assets/img/historia/gallinas.jpg" alt="Gallina">
-
-        </div>
-    </div>
-
-    <!-- Empieza seccion -->
-
-    <div class="spce-c mb-5">
-      <div class="col-lg-4 content-image">
-          <img class="d-block w-100" src="assets/img/historia/mv.jpg" alt="Misión y Vsión">
+   </div>
+   <div class="col-lg-4 content-image">
+      <img class="d-block w-100" src="assets/img/historia/gallinas.jpg" alt="Gallina">
+   </div>
+</div>
+<!-- Empieza seccion -->
+<div class="spce-c mb-5">
+   <div class="col-lg-4 content-image">
+      <img class="d-block w-100" src="assets/img/historia/mv.jpg" alt="Misión y Vsión">
+   </div>
+   <div class="col-lg-4 mgt-2">
+      <div class="col-lg-10 txt-justify">
+         <section class="mx-auto pt-3 spce-c  mb-5">
+            <span class="content-titles">
+               <h3 class="title-center txt-c">MISIÓN</h3>
+            </span>
+         </section>
+         <p>Colocar en el mercado productos alimenticios, garantizando la satisfacción de nuestros clientes, personal y accionistas.</p>
+         <section class="mx-auto pt-3 spce-c mb-5">
+            <span class="content-titles">
+               <h3 class="title-center txt-c">VISIÓN</h3>
+            </span>
+         </section>
+         <p >Ser una empresa de clase mundial que genere valor a su entorno.</p>
       </div>
-        <div class="col-lg-4 mgt-2">
-            <div class="col-lg-10 txt-justify">
-              <section class="mx-auto pt-3 spce-c  mb-5">
-                <span class="content-titles">
-                  <h3 class="title-center txt-c">MISIÓN</h3>
-                </span>
-              </section>
-              <p>Colocar en el mercado productos alimenticios, garantizando la satisfacción de nuestros clientes, personal y accionistas.</p>
-              <section class="mx-auto pt-3 spce-c mb-5">
-                <span class="content-titles">
-                <h3 class="title-center txt-c">VISIÓN</h3>
-                </span>
-              </section>
-              <p >Ser una empresa de clase mundial que genere valor a su entorno.</p>
-                
-            </div>
-        </div>
-    </div>
-    <!-- Empieza seccion -->
-    <div class="spce-c mb-5">
-      
-        <div class="col-lg-4 mgt-2">
-            <div class="col-lg-10 txt-justify">
-              <section class="mx-auto pt-3 spce-c  mb-5">
-                <span class="content-titles">
-                  <h3 class="title-center txt-c">EMPRESA</h3>
-                </span>
-              </section>
-              <p >Nos comprometemos a brindar un producto de calidad y el mejor precio, para que cada hogar tenga una comida aceptable y de calidad.</p>
-              <section class="mx-auto pt-3 spce-c mb-5">
-                <span class="content-titles">
-                <h3 class="title-center txt-c">POLÍTICA DE CALIDAD</h3>
-                </span>
-              </section>
-              <p >En Ojai Alimentos, la meta diaria es ofrecer a nuestros clientes productos con los mas altos estándares de calidad, que logren su satisfacción total, a través del mejoramiento continuo de nuestros procesos y un liderazgo comprometido desde la alta dirección.</p>
-                
-            </div>
-        </div>
-        <div class="col-lg-4 content-image">
-          <img class="d-block w-100" src="assets/img/historia/pc.jpg" alt="Misión y Vsión">
+   </div>
+</div>
+<!-- Empieza seccion -->
+<div class="spce-c mb-5">
+   <div class="col-lg-4 mgt-2">
+      <div class="col-lg-10 txt-justify">
+         <section class="mx-auto pt-3 spce-c  mb-5">
+            <span class="content-titles">
+               <h3 class="title-center txt-c">EMPRESA</h3>
+            </span>
+         </section>
+         <p >Nos comprometemos a brindar un producto de calidad y el mejor precio, para que cada hogar tenga una comida aceptable y de calidad.</p>
+         <section class="mx-auto pt-3 spce-c mb-5">
+            <span class="content-titles">
+               <h3 class="title-center txt-c">POLÍTICA DE CALIDAD</h3>
+            </span>
+         </section>
+         <p >En Ojai Alimentos, la meta diaria es ofrecer a nuestros clientes productos con los mas altos estándares de calidad, que logren su satisfacción total, a través del mejoramiento continuo de nuestros procesos y un liderazgo comprometido desde la alta dirección.</p>
       </div>
-    </div>
-
-
-
-
+   </div>
+   <div class="col-lg-4 content-image">
+      <img class="d-block w-100" src="assets/img/historia/pc.jpg" alt="Misión y Vsión">
+   </div>
+</div>
 <!-- Fin spec-c -->
 <div class="box box-right">
-
 <section class="col-lg-10 mx-auto spce-c">
-        <span class="content-titles">
-        <h3 class="title-center txt-c">NUESTROS VALORES</h3>
-        </span>
-
+   <span class="content-titles">
+      <h3 class="title-center txt-c">NUESTROS VALORES</h3>
+   </span>
 </section>
-
-
 <div class="spce-c">
-  <div class="col-lg-8 mgt-2">
-  <div class="spce-h mgt-2">
-                <div class="fila">
-                    <div class="juntos spce-h">
-                      <p><i class="fas fa-award"></i>Responsabilidad</p>
-                      <p><i class="fas fa-hand-paper"></i>Honestidad</p>
-                      <p><i class="fas fa-handshake"></i>Lealtad</p>
-                    </div>
-                    <div class="juntos">
-                      <p><i class="fas fa-users"></i>Trabajo en equipo</p>
-                      <p><i class="fas fa-business-time"></i></i>Disciplina</p>
-                    </div>
-                      <div class="juntos">
-                        <p><i class="fas fa-people-carry"></i></i>Actitud de servicio</p>
-                        <p><i class="fas fa-praying-hands"></i></i>Perseverancia</p>
-                      </div>
-                </div>
-    </div> <!-- fin box -->     
-  </div>
+   <div class="col-lg-8 mgt-2">
+      <div class="spce-h mgt-2">
+         <div class="fila">
+            <div class="juntos spce-h">
+               <p><i class="fas fa-award"></i>Responsabilidad</p>
+               <p><i class="fas fa-hand-paper"></i>Honestidad</p>
+               <p><i class="fas fa-handshake"></i>Lealtad</p>
+            </div>
+            <div class="juntos">
+               <p><i class="fas fa-users"></i>Trabajo en equipo</p>
+               <p><i class="fas fa-business-time"></i></i>Disciplina</p>
+            </div>
+            <div class="juntos">
+               <p><i class="fas fa-people-carry"></i></i>Actitud de servicio</p>
+               <p><i class="fas fa-praying-hands"></i></i>Perseverancia</p>
+            </div>
+         </div>
+      </div>
+      <!-- fin box -->     
+   </div>
 </div>
 <!-- Contadores -->
 <section class="spce-c pdding-40">
-  <span class="content-titles">
-    <h3 align="center"  class="title-center">COMPROMISO</h3>
-  </span>
+   <span class="content-titles">
+      <h3 align="center"  class="title-center">COMPROMISO</h3>
+   </span>
 </section>
-  <section id="section-contadores" class="parallax section section-text-light section-parallax section-center mt-0" data-plugin-parallax data-plugin-options="{'speed': 1.5}" data-image-src="img/parallax/parallax-2.jpg">
-          <img class="img-parallax" src="assets/img/parallax/table-parallax" alt="">
-					<div class="container">
-						<div class="row counters counters-text-light">
-							<div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
-								<div class="counter">
-                  <!-- <strong data-to="30000" data-append="+">0</strong> -->
-                  <strong>+</strong>
-									<i class="fas fa-user"></i>
-									<strong class="cont" data-to="800" data-append="+">0</strong>
-									<label>Clientes satisfechos</label>
-								</div>
-							</div>
-							<div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
-								<div class="counter">
-                  <strong>+</strong>
-                  <i class="fas fa-star"></i>
-									<strong class="cont" data-to="15">0</strong>
-									<label>Años en el negocio</label>
-								</div>
-							</div>
-							<div class="col-sm-6 col-lg-3 mb-4 mb-sm-0">
-								<div class="counter">
-                  <strong>+</strong>
-									<i class="fas fa-building"></i>
-									<strong class="cont" data-to="20">0</strong>
-									<label>Sucursales</label>
-								</div>
-							</div>
-							<div class="col-sm-6 col-lg-3">
-								<div class="counter">
-                  <strong>+</strong>
-									<i class="fas fa-egg"></i>
-									<strong class="cont" data-to="40">0</strong>
-									<label>Productos</label>
-								</div>
-							</div>
-						</div>
-					</div>
-        </section>
-
+<section id="section-contadores" class="parallax section section-text-light section-parallax section-center mt-0" data-plugin-parallax data-plugin-options="{'speed': 1.5}" data-image-src="img/parallax/parallax-2.jpg">
+   <img class="img-parallax" src="assets/img/parallax/table-parallax" alt="">
+   <div class="container">
+      <div class="row counters counters-text-light">
+         <div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
+            <div class="counter">
+               <!-- <strong data-to="30000" data-append="+">0</strong> -->
+               <strong>+</strong>
+               <i class="fas fa-user"></i>
+               <strong class="cont" data-to="800" data-append="+">0</strong>
+               <label>Clientes satisfechos</label>
+            </div>
+         </div>
+         <div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
+            <div class="counter">
+               <strong>+</strong>
+               <i class="fas fa-star"></i>
+               <strong class="cont" data-to="15">0</strong>
+               <label>Años en el negocio</label>
+            </div>
+         </div>
+         <div class="col-sm-6 col-lg-3 mb-4 mb-sm-0">
+            <div class="counter">
+               <strong>+</strong>
+               <i class="fas fa-building"></i>
+               <strong class="cont" data-to="20">0</strong>
+               <label>Sucursales</label>
+            </div>
+         </div>
+         <div class="col-sm-6 col-lg-3">
+            <div class="counter">
+               <strong>+</strong>
+               <i class="fas fa-egg"></i>
+               <strong class="cont" data-to="40">0</strong>
+               <label>Productos</label>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
 <section class="col-lg-10 mx-auto spce-c">
-        <span class="content-titles">
-        <h3 class="title-center txt-c">Gente Ojai</h3>
-        </span>
+   <span class="content-titles">
+      <h3 class="title-center txt-c">Gente Ojai</h3>
+   </span>
 </section>
-
 <div class="row spce-c pt-2 m-3 txt-j">
-    <div class="col-lg-8 spce-c">
-        <div class="col-lg-12">
-            <p class="pt-3" >
- En Ojai Alimentos buscamos la excelencia en nuestros resultados y forjamos relaciones perdurables basadas en la confianza, 
- al vivir con intensidad nuestros valores esenciales: Honestidad, Responsabilidad, Disciplina, 
- Lealtad, Trabajo en equipo, Perseverancia y Actitud de servicio.
-</p>
-<p>
- Una cultura basada en ello 
- contribuye a incrementar, de manera sostenible, el valor de la compañía para todos los grupos 
- de interés: clientes, accionistas, personal y en la comunidad donde nos desarrollamos. 
- </p>
- <p>
-    De esta manera podemos asegurar que llevaremos a cabo nuestra Visión y nuestra Misión.
-    Sabemos que para lograrlo, cada uno de las personas que trabajamos en la compañía constituye una
-    pieza importante que aporta su esfuerzo, energía y pasión para cumplir cabalmente con la 
-    responsabilidad que le ha sido asignada, así es nuestro equipo y estamos orgullosos de que 
-    basamos el éxito de la empresa en el activo más importante: Nuestra Gente. </p>
-        </div>
-      
-    </div>
+   <div class="col-lg-8 spce-c">
+      <div class="col-lg-12">
+         <p class="pt-3" >
+            En Ojai Alimentos buscamos la excelencia en nuestros resultados y forjamos relaciones perdurables basadas en la confianza, 
+            al vivir con intensidad nuestros valores esenciales: Honestidad, Responsabilidad, Disciplina, 
+            Lealtad, Trabajo en equipo, Perseverancia y Actitud de servicio.
+         </p>
+         <p>
+            Una cultura basada en ello 
+            contribuye a incrementar, de manera sostenible, el valor de la compañía para todos los grupos 
+            de interés: clientes, accionistas, personal y en la comunidad donde nos desarrollamos. 
+         </p>
+         <p>
+            De esta manera podemos asegurar que llevaremos a cabo nuestra Visión y nuestra Misión.
+            Sabemos que para lograrlo, cada uno de las personas que trabajamos en la compañía constituye una
+            pieza importante que aporta su esfuerzo, energía y pasión para cumplir cabalmente con la 
+            responsabilidad que le ha sido asignada, así es nuestro equipo y estamos orgullosos de que 
+            basamos el éxito de la empresa en el activo más importante: Nuestra Gente. 
+         </p>
+      </div>
+   </div>
 </div>
-
 <!-- <div class="row-flex mgt-5">
-    <img src="assets/img/nuestros_valores.jpg" class="img-100 p-2" alt="Mision">
-    
-</div>  -->
+   <img src="assets/img/nuestros_valores.jpg" class="img-100 p-2" alt="Mision">
+   
+   </div>  -->
 <!-- Fin spec-c -->
 
 
