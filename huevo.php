5ae91bff2a7cd0f2ea7e55cf04410d80883cf7c3
Javier Felix <desarrollo.ti@CORPORATIVOOJAI.COM>
2020-05-21 18:48:46 -0700

    Se agregaron cambios en la pagina de cerdo y en la de huevo

--- huevo.php
+++ huevo.php
@@ -8,48 +8,37 @@
 </section>
 <div class="row mb-5"></div>
     <div>
-    <p>   A diferencia de un sistema conevncional, en el sistema libre de jaula, las gallinas pueden caminar libremente por la caseta y usar los nidos para dormir y poner sus huevos
-    .Este sistema ayuda alas gallinas a relajarse, hacer ekercicio y a socializar entre ellas, lo cual asegura la calidad de sus huevos.
-    </p>
-    <h5><b>4 razones por que comer huevo de gallina libre:</b></h5>
-    <ul>
-        <li><b>-</b>  Buenas condiciones de animales.</li>
-        <li><b>-</b>  Gallinas felices. ponen huevos saludables.</li>
-        <li><b>-</b>  Apoyo a las empresas locales.</li>
-        <li><b>-</b>  Contribuir a un movimiento sustentable.</li>
-    </ul>
-    <p><b>Ojai Alimentos</b> se unió a la iniciativa de huevo de gallina libre de jaula con el fin de ser una empresa que promueve el amor y el cuidado de sus animalesy
+    <h5><b>GALLINA </b>LIBRE</h5>
+    
+   
+    <p>Ojai Alimentos se unió a la iniciativa de huevo de gallina libre de jaula con el fin de ser una empresa que promueve el amor y el cuidado de sus animalesy
     y sobre todo ser socialmente responsable con la comunidad.</p>
     </div>
 
-    <div>
-    <h5><b>CALIDAD AVÍCOLA</b>& CULTURA ORGANIZACIONAL</h5>
-    <p>La apuesta por  nuestro personal y la calidad ed nuestro produto como diferenciación de nuestra competencia, es lograr que en nuestros procesos de trabajo contemos
-    con las herramientas, conocimiento, habilidades y sistemas que contribuyan a una labor diaria por poner en manos de nuestros clientes internos y extrnos productos alimenticios de calidad.</p>
-    </div>
 
-
-    <div class="row mb-3">
+    <div class="row mb-3 txt-justify">
     <div class="col-sm">
-    <h5><b>PRODUCCIÓN </b>AVICOLA</h5>
-    <p> La empresa a partir del 2016 apostó por la calidad del valor agregado en nuestro producto Huevo para Plato.
-        Al mismo tiempo se amplia la capacidad de infraestructura en Avicola 5.</p>
+    <h5>BUENAS CONDICIONES DE ANIMALES</h5>
+    <p> A diferencia de un sistema conevncional, en el sistema libre de jaula, las gallinas pueden caminar libremente por la caseta y usar los nidos para dormir y poner sus huevos
+    .Este sistema ayuda alas gallinas a relajarse, hacer ekercicio y a socializar entre ellas, lo cual asegura la calidad de sus huevos.</p>
     </div>
     <div class="col-sm">
-    <h5><b>INAUGURACIÓN </b>CD. OBREGÓN</h5>
-    <p>Se realiza cambio de infraestructura sucursal huevo Obregón, contando con la participación del equipo directivo y administración de sucursal.</p>
+    <h5>MAS NUTRITIVOS</h5>
+    <p>Una gallina sana produce un huevo sano, no hay mucha ciencia en ello. Sin embargo tiene muchos y mejores beneficios los huevos de gallina libre respecto a otros:
+     1/3 menos colesterol, 1/4 menos de grasas saturadas, 2/3 más de vitamina A, 2 veces más ácidos grasos omega-3, 3 veces más vitamina E, 7 veces más beta caroteno.
+    </p>
     </div>
   </div>
 
-  <div class="row mb-3">
+  <div class="row mb-3 txt-justify">
     <div class="col-sm">
-    <h5><b>COMERCIAL</b>HUEVO</h5>
-    <p>Se aperturan las sucursales para la comercialización directa del producto huevo en las plazas Juáres y Chihuahua, logrando posicionamiento de nuestra marca.</p>
+    <h5>MEJOR SABOR</h5>
+    <p>Los huevos son más frescos y más sanos ya que vienen de gallinas felices, ademas de que no huelen mal, incluso no dejan malos olores en tus platos ni en el mismo sartén que se concinó.</p>
     
     </div>
     <div class="col-sm">
-    <h5><b>COMERCIAL</b>CARNE</h5>
-    <p>Logrando una participación de mercado en el  producto cárnico se inicia con la operación de venta de Sucursal Culiacán.</p>
+    <h5>SALUD CARDIOVASCULAR</h5>
+    <p>Aporta energía a través de ácidos grasos monoinsanturados y poliinsaturados, también conocidas como “grasas saludables”..</p>
     
     </div>
 
@@ -77,8 +66,8 @@
 <div class="content-timeline">
     <div class="line-center"></div>
     <div class="wrapper  mx-img">
-        <div class="content-item item1  animAparece item-center">
-        <div class="bg-main  item-card ">
+        <div class="content-item item1  animAparece item-center ">
+        <div class="bg-main  item-card txt-justify">
             <h2>Granjas de Postura</h2>
             <p>OJAI Alimentos cuenta con 17 granjas de postura comercial, aquí es donde tenemos a nuestras aves en edad productiva que producen huevo blanco para plato, las cuales están ubicadas principalmente en el valle del yaqui muy cerca de Cd. Obregón donde tenemos el 55% de nuestra producción con 1’077,560 aves, además de nuestras granjas en Hermosillo, donde tenemos el 13% de la producción gracias a 258,000 aves y 634,980 aves en Los Mochis, que representan el 32 % de nuestra producción total.</p>
         </div>
@@ -98,8 +87,8 @@
             
 
         </div>
-        <div class="content-item item4 aparece item-center">
-            <div class="bg-main  item-card">
+        <div class="content-item item4 aparece item-center ">
+            <div class="bg-main  item-card txt-justify">
             <h2>Granjas de crianza</h2>
                 <p>Para iniciar con el proceso de producción de huevo en Ojai Alimentos se cuenta con cuatro granjas de crianza donde nuestras aves permanecen desde el primer día de vida hasta las 18 semanas de edad. En esta etapa, el ave pasa de ser pollita a gallina joven, llega a su madurez sexual y se prepara para iniciar su vida productiva.</p>
                 <p>Se tiene una capacidad de crianza de 510,000 pollitas en dos unidades donde se están desarrollando las futuras productoras de huevo de nuestra compañía.</p>
@@ -119,8 +108,8 @@
             
 
         </div>
-        <div class="content-item item7 aparece item-center">
-            <div class="bg-main  item-card">
+        <div class="content-item item7 aparece item-center ">
+            <div class="bg-main  item-card txt-justify">
             <h2>Capacidad</h2>
                 <p>Contamos con capacidad para albergar 1’970,540 aves que nos permiten tener una producción anual de 31, 000 toneladas de huevo. Es de suma importancia reiterar que el 100% de nuestras granjas tienen la certificación por parte de SAGARPA de libres enfermedades exóticas como influenza aviar, Newcastle y salmonella. A demás, toda la producción de huevo en nuestra región cuenta con reconocimiento de libre de enfermedades exóticas de parte de USDA, por lo que estamos certificados para exportar huevo para plato a EEUU.</p>
             </div>
